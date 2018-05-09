c8c0a6b48dc3900556104af5a9772474667a8098
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-05-09 03:26:05 +0200

    Assets añadidos y trabajo de las dos primeras clases prácticas

---
+++ Assets/Project/Scripts/Scr_CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class Scr_CameraController : MonoBehaviour
+{
+    public float camSens = 0.50f; // Sensibilidad con el uso de ratón
+    Vector3 lastMouse = new Vector3(255, 255, 255);
+    [SerializeField] GameObject bullet;
+
+    private void Update()
+    {
+        if (!UnityEngine.XR.XRSettings.enabled)
+            CameraPosition();
+
+        if (Input.GetButtonDown("Fire1"))
+            ShootABullet();
+    }
+
+    void CameraPosition()
+    {
+        lastMouse = Input.mousePosition - lastMouse;
+        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse = Input.mousePosition;
+    }
+
+    void ShootABullet()
+    {
+        GameObject bulletInstance = Instantiate(bullet, transform.position, transform.rotation);
+        Scr_BulletHandler bh = bulletInstance.GetComponent<Scr_BulletHandler>();
+
+        if (bh != null)
+            bh.Shoot(transform.forward);
+        else
+            Debug.Log("La bullet no tiene adjunto el script bulletHandler");
+
+        Destroy(bulletInstance, 3f);
+    }
+}
