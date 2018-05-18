fafdcef8d44d8e54f19489d7ac07b1e047ea3f76
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-05-18 14:18:57 -0800

    Sábado 18

--- Assets/Project/Scripts/Scr_GameplayManager.cs
+++ Assets/Project/Scripts/Scr_GameplayManager.cs
@@ -1,29 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 
 public class Scr_GameplayManager : MonoBehaviour
 {
+    [SerializeField] TextMeshPro textMesh;
+    [SerializeField] float time = 60;
+
+    [HideInInspector]
+    public int points = 0;
+
     private static Scr_GameplayManager instance;
 
-    private float timeZero;
-    public float playingTime = 60f;
-    public TextMeshPro textMesh;
-
-    public float points = 0f;
+    Scr_GameManager gameManager;
 
     private void Start()
     {
-        timeZero = Time.realtimeSinceStartup;
         instance = this;
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<Scr_GameManager>();
     }
 
     private void Update()
     {
-        if (Time.realtimeSinceStartup > (playingTime + timeZero))
-            Application.Quit();
-        textMesh.text = "Time " + (int)(playingTime - (Time.realtimeSinceStartup - timeZero)) + "           " + "Points " + points;
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            gameManager.NewHighscore(points);
+
+            SceneManager.LoadScene(0);
+        }
+
+        textMesh.text = "Time " + (int) time + "           " + "Points " + points;
     }
 
     public static Scr_GameplayManager GetInstance()
